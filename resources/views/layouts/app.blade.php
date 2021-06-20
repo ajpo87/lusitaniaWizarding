04799e5ae43632d2912b909816b70c23df9f3948
ajpo87 <antonio.oliveira87@gmail.com>
2021-06-20 19:48:40 +0100

    Fix toggle responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                    Lusitania Wizarding
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}" style="background:goldenrod">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
